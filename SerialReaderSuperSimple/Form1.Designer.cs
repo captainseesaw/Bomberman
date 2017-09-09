c6963216cdb43b51a5ae100329036501e541ef7e
Maxwell Li <li-maxwell@outlook.com>
2017-09-09 13:34:04 -0700

    init commit

---
+++ SerialReaderSuperSimple/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SerialReaderSuperSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtZ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(12, 21);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 28);
+            this.cmbPorts.TabIndex = 0;
+            this.cmbPorts.SelectionChangeCommitted += new System.EventHandler(this.cmbPorts_SelectionChangeCommitted);
+            this.cmbPorts.Click += new System.EventHandler(this.cmbPorts_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(155, 21);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(111, 37);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(15, 70);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(108, 26);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(15, 112);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(108, 26);
+            this.txtY.TabIndex = 3;
+            // 
+            // txtZ
+            // 
+            this.txtZ.Location = new System.Drawing.Point(14, 154);
+            this.txtZ.Name = "txtZ";
+            this.txtZ.Size = new System.Drawing.Size(108, 26);
+            this.txtZ.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X axis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y axis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Z axis";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 15;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtZ);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer;
+        private System.IO.Ports.SerialPort serialPort;
+    }
+}
+
---
+++ SerialReaderSuperSimple/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Collections.Concurrent;
+
+enum Pos {X,Y,Z,OVER}
+
+namespace SerialReaderSuperSimple
+{
+    public partial class Form1 : Form
+    {
+        int baudRate = 128000;
+        int dataBits = 8; //bits
+
+        // dataQueue
+        ConcurrentQueue<int> dataQueue = new ConcurrentQueue<int>();
+
+        // case idx
+        int idx = 0;
+        // item limit
+        int itemLimit = 8;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // initialize serial port
+            serialPort.BaudRate = baudRate;
+            serialPort.DataBits = dataBits;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+        }
+
+        private void cmbPorts_Click(object sender, EventArgs e)
+        {
+            cmbPorts.Items.Clear();
+            cmbPorts.Items.AddRange(SerialPort.GetPortNames().ToArray());
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cmbPorts.SelectedIndex == -1)
+            {
+                return;
+            }
+  
+            bool connected = serialPort.IsOpen;
+            if (connected) { 
+                serialPort.Close();
+                btnConnect.Text = "Connect";
+                Console.WriteLine("disconnected");
+                cmbPorts.Enabled = true;
+                // stop timer
+                timer.Stop();
+                dataQueue = new ConcurrentQueue<int>();
+            }
+            else
+            {
+                serialPort.Open();
+                btnConnect.Text = "Disconnect";
+                Console.WriteLine("connected");
+                cmbPorts.Enabled = false;
+                // start timer
+                timer.Start();
+            }
+        }
+
+        private void cmbPorts_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            serialPort.PortName = cmbPorts.SelectedItem.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort.Close();
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // read all available bytes
+            int numToRead = serialPort.BytesToRead;
+            byte[] arr = new byte[numToRead];
+            serialPort.Read(arr,0, numToRead);
+
+            foreach (int data in arr)
+            {
+                if (dataQueue.Count > itemLimit)
+                    dataQueue.TryDequeue(out int trash);
+                dataQueue.Enqueue(data);
+                //Console.WriteLine(data);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int data;
+
+            // check if queue is empty
+            if (dataQueue.Count == 0)
+                return;
+            else
+            {
+                dataQueue.TryDequeue(out data);
+                // read a packet of data
+                // first find 255
+                while (data != 255)
+                {
+                    dataQueue.TryDequeue(out data);
+                }
+                // display x, y, z
+                for (Pos curr =  Pos.X; curr != Pos.OVER; curr++)
+                {
+                    dataQueue.TryDequeue(out data);
+                    switch (curr)
+                    {
+                        case Pos.X:
+                            txtX.Text = data.ToString();
+                            break;
+                        case Pos.Y:
+                            txtY.Text = data.ToString();
+                            break;
+                        case Pos.Z:
+                            txtZ.Text = data.ToString();
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+}
