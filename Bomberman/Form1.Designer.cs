80623330803f7040040973aa1776f9a66673a41c
Maxwell Li <li-maxwell@outlook.com>
2017-09-11 09:00:57 -0700

    added plot, fixed dequeue from two threads

--- Bomberman/Form1.Designer.cs
+++ Bomberman/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.cmbPorts = new System.Windows.Forms.ComboBox();
             this.btnConnect = new System.Windows.Forms.Button();
             this.txtX = new System.Windows.Forms.TextBox();
@@ -39,6 +45,27 @@
             this.label3 = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.txtOrientation = new System.Windows.Forms.TextBox();
+            this.lblOrientation = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtZAvg = new System.Windows.Forms.TextBox();
+            this.txtYAvg = new System.Windows.Forms.TextBox();
+            this.txtXAvg = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtGesture = new System.Windows.Forms.TextBox();
+            this.txtLED = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtZThreshold = new System.Windows.Forms.TextBox();
+            this.txtYThreshold = new System.Windows.Forms.TextBox();
+            this.txtXThreshold = new System.Windows.Forms.TextBox();
+            this.chrAcc = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chrAcc)).BeginInit();
             this.SuspendLayout();
             // 
             // cmbPorts
@@ -111,18 +138,225 @@
             // 
             // timer
             // 
-            this.timer.Interval = 15;
+            this.timer.Interval = 10;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // serialPort
             // 
-            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort_DataReceived);
+            // 
+            // txtOrientation
+            // 
+            this.txtOrientation.Location = new System.Drawing.Point(15, 233);
+            this.txtOrientation.Name = "txtOrientation";
+            this.txtOrientation.Size = new System.Drawing.Size(251, 26);
+            this.txtOrientation.TabIndex = 8;
+            // 
+            // lblOrientation
+            // 
+            this.lblOrientation.AutoSize = true;
+            this.lblOrientation.Location = new System.Drawing.Point(20, 204);
+            this.lblOrientation.Name = "lblOrientation";
+            this.lblOrientation.Size = new System.Drawing.Size(91, 20);
+            this.lblOrientation.TabIndex = 9;
+            this.lblOrientation.Text = "Orientation:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Z avg";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 336);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Y avg";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "X avg";
+            // 
+            // txtZAvg
+            // 
+            this.txtZAvg.Location = new System.Drawing.Point(14, 374);
+            this.txtZAvg.Name = "txtZAvg";
+            this.txtZAvg.Size = new System.Drawing.Size(108, 26);
+            this.txtZAvg.TabIndex = 12;
+            // 
+            // txtYAvg
+            // 
+            this.txtYAvg.Location = new System.Drawing.Point(15, 332);
+            this.txtYAvg.Name = "txtYAvg";
+            this.txtYAvg.Size = new System.Drawing.Size(108, 26);
+            this.txtYAvg.TabIndex = 11;
+            // 
+            // txtXAvg
+            // 
+            this.txtXAvg.Location = new System.Drawing.Point(15, 290);
+            this.txtXAvg.Name = "txtXAvg";
+            this.txtXAvg.Size = new System.Drawing.Size(108, 26);
+            this.txtXAvg.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(181, 418);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "N";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(15, 415);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(108, 26);
+            this.txtN.TabIndex = 16;
+            this.txtN.Text = "100";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(377, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 20);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Gesture:";
+            // 
+            // txtGesture
+            // 
+            this.txtGesture.Location = new System.Drawing.Point(372, 51);
+            this.txtGesture.Name = "txtGesture";
+            this.txtGesture.Size = new System.Drawing.Size(251, 26);
+            this.txtGesture.TabIndex = 18;
+            // 
+            // txtLED
+            // 
+            this.txtLED.BackColor = System.Drawing.Color.Red;
+            this.txtLED.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLED.Location = new System.Drawing.Point(446, 95);
+            this.txtLED.Multiline = true;
+            this.txtLED.Name = "txtLED";
+            this.txtLED.Size = new System.Drawing.Size(94, 85);
+            this.txtLED.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(547, 288);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 20);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Z Threshold";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(547, 247);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Y Threshold";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(547, 204);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 20);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "X Threshold";
+            // 
+            // txtZThreshold
+            // 
+            this.txtZThreshold.Location = new System.Drawing.Point(380, 285);
+            this.txtZThreshold.Name = "txtZThreshold";
+            this.txtZThreshold.Size = new System.Drawing.Size(108, 26);
+            this.txtZThreshold.TabIndex = 23;
+            this.txtZThreshold.Text = "230";
+            // 
+            // txtYThreshold
+            // 
+            this.txtYThreshold.Location = new System.Drawing.Point(381, 243);
+            this.txtYThreshold.Name = "txtYThreshold";
+            this.txtYThreshold.Size = new System.Drawing.Size(108, 26);
+            this.txtYThreshold.TabIndex = 22;
+            this.txtYThreshold.Text = "200";
+            // 
+            // txtXThreshold
+            // 
+            this.txtXThreshold.Location = new System.Drawing.Point(381, 201);
+            this.txtXThreshold.Name = "txtXThreshold";
+            this.txtXThreshold.Size = new System.Drawing.Size(108, 26);
+            this.txtXThreshold.TabIndex = 21;
+            this.txtXThreshold.Text = "220";
+            // 
+            // chrAcc
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chrAcc.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend";
+            this.chrAcc.Legends.Add(legend6);
+            this.chrAcc.Location = new System.Drawing.Point(372, 336);
+            this.chrAcc.Name = "chrAcc";
+            series16.ChartArea = "ChartArea1";
+            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series16.Legend = "Legend";
+            series16.Name = "X";
+            series17.ChartArea = "ChartArea1";
+            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series17.Legend = "Legend";
+            series17.Name = "Y";
+            series18.ChartArea = "ChartArea1";
+            series18.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series18.Legend = "Legend";
+            series18.Name = "Z";
+            this.chrAcc.Series.Add(series16);
+            this.chrAcc.Series.Add(series17);
+            this.chrAcc.Series.Add(series18);
+            this.chrAcc.Size = new System.Drawing.Size(575, 300);
+            this.chrAcc.TabIndex = 27;
+            title6.Name = "Acceleartion Plot";
+            title6.Text = "Acceleration Plot";
+            this.chrAcc.Titles.Add(title6);
             // 
             // monitorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 202);
+            this.ClientSize = new System.Drawing.Size(977, 656);
+            this.Controls.Add(this.chrAcc);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtZThreshold);
+            this.Controls.Add(this.txtYThreshold);
+            this.Controls.Add(this.txtXThreshold);
+            this.Controls.Add(this.txtLED);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtGesture);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtZAvg);
+            this.Controls.Add(this.txtYAvg);
+            this.Controls.Add(this.txtXAvg);
+            this.Controls.Add(this.lblOrientation);
+            this.Controls.Add(this.txtOrientation);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -134,6 +368,7 @@
             this.Name = "monitorForm";
             this.Text = "monitorForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.chrAcc)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +386,26 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Timer timer;
         private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox txtOrientation;
+        private System.Windows.Forms.Label lblOrientation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtZAvg;
+        private System.Windows.Forms.TextBox txtYAvg;
+        private System.Windows.Forms.TextBox txtXAvg;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtGesture;
+        private System.Windows.Forms.TextBox txtLED;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtZThreshold;
+        private System.Windows.Forms.TextBox txtYThreshold;
+        private System.Windows.Forms.TextBox txtXThreshold;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrAcc;
     }
 }
 
--- Bomberman/Form1.cs
+++ Bomberman/Form1.cs
@@ -9,21 +9,43 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
 
-enum Pos {X,Y,Z,OVER}
+enum READ_ACC {X,Y,Z,OVER}
+enum GESTURE {IDLE, START, ACQUIRE, Processing, OVER}
 
 namespace Bomberman
 {
     public partial class monitorForm : Form
     {
+        // threshold for orientation
+        int ORIENTATION_LOW = 105;
+        int ORIENTATION_UP = 155;
+
+        // serial port
         int baudRate = 128000;
         int dataBits = 8; //bits
 
         // dataQueue
         ConcurrentQueue<int> dataQueue = new ConcurrentQueue<int>();
+        int itemLimit = 8; // item limit
 
-        // item limit
-        int itemLimit = 8;
+        // list for x,y,z values
+        List<int> x_acc = new List<int>();
+        List<int> y_acc = new List<int>();
+        List<int> z_acc = new List<int>();
+
+        // gesture reading
+        Stopwatch gestureWatch = new Stopwatch();
+        GESTURE gestureState = GESTURE.IDLE;
+        string readGesture = "";
+        int gestureTimeout = 2000;
+        int displayTimeout = 1000;
+
+        // plot 
+        int count = 0;
+
 
         public monitorForm()
         {
@@ -80,7 +102,7 @@ namespace Bomberman
             serialPort.Close();
         }
 
-        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             // read all available bytes
             int numToRead = serialPort.BytesToRead;
@@ -89,11 +111,137 @@ namespace Bomberman
 
             foreach (int data in arr)
             {
-                if (dataQueue.Count > itemLimit)
-                    dataQueue.TryDequeue(out int trash);
+                //while (dataQueue.Count > itemLimit)
+                //    dataQueue.TryDequeue(out int trash);
                 dataQueue.Enqueue(data);
                 //Console.WriteLine(data);
             }
+        }
+
+        private void Update_orientation(int xData, int yData, int zData)
+        {
+            // update orientation
+            if (xData > ORIENTATION_UP)
+                txtOrientation.Text = "face downward";
+            else if (xData < ORIENTATION_LOW)
+                txtOrientation.Text = "face forward";
+            else if (yData > ORIENTATION_UP)
+                txtOrientation.Text = "face left";
+            else if (yData < ORIENTATION_LOW)
+                txtOrientation.Text = "face right";
+            else if (zData > ORIENTATION_UP)
+                txtOrientation.Text = "face up";
+            else if (zData < ORIENTATION_LOW)
+                txtOrientation.Text = "face down";
+            else
+                txtOrientation.Text = String.Empty;
+        } 
+
+        private void Calculate_average(int avgCount)
+        {
+            // calculate average
+            if (avgCount != 0)
+            {
+                txtXAvg.Text = (x_acc.Sum() / x_acc.Count).ToString();
+                txtYAvg.Text = (y_acc.Sum() / y_acc.Count).ToString();
+                txtZAvg.Text = (z_acc.Sum() / z_acc.Count).ToString();
+            }
+            else
+            {
+                txtXAvg.Text = String.Empty;
+                txtYAvg.Text = String.Empty;
+                txtZAvg.Text = String.Empty;
+            }
+        }
+
+        private void Update_gesture(int xData, int yData, int zData,
+            int xThreshold, int yThreshold, int zThreshold)
+        {
+            switch (gestureState)
+            {
+                case (GESTURE.START):
+                    gestureWatch.Start();
+                    txtLED.BackColor = Color.Green;
+                    gestureState = GESTURE.ACQUIRE;
+                    break;
+                case (GESTURE.ACQUIRE):
+                    if (gestureWatch.ElapsedMilliseconds > gestureTimeout)
+                    {
+                        gestureState = GESTURE.OVER;
+                        Console.WriteLine("over 2s, restart");
+                    }
+                    else if (gestureWatch.ElapsedMilliseconds > displayTimeout)
+                    {
+                        txtGesture.Clear();
+                        Console.WriteLine("over 1s, display clear");
+                    }
+                    if ( readGesture.Length <= 3)
+                    {
+                        if (xData > xThreshold && !readGesture.EndsWith("X"))
+                        {
+                            readGesture += "X";
+                            txtGesture.Text = readGesture;
+                        }
+
+                        if (yData > yThreshold && !readGesture.EndsWith("Y") && readGesture.Contains('X'))
+                        {
+                            readGesture += "Y";
+                            txtGesture.Text = readGesture;
+                        }
+
+                        if (zData > zThreshold && !readGesture.EndsWith("Z") && !readGesture.Contains("ZX"))
+                        {
+                            readGesture += "Z";
+                            txtGesture.Text = readGesture;
+                        }
+                    }
+                    switch (readGesture)
+                    {
+                        case "X":
+                            txtGesture.Text = "Simple punch";
+                            break;
+                        case "ZX":
+                            txtGesture.Text = "High punch";
+                            break;
+                        case "XYZ":
+                            txtGesture.Text = "Right-hook";
+                            break;
+                    }
+                    //Console.WriteLine(readGesture);
+                    break;
+                case (GESTURE.OVER):
+                    txtGesture.Clear();
+                    txtLED.BackColor = Color.Red;
+                    gestureWatch.Reset();
+                    gestureState = GESTURE.IDLE;
+                    readGesture = "";
+                    Console.WriteLine("over 1s, clear");
+                    
+                    break;
+            }
+        }
+
+        private void Update_plot (int xData, int yData, int zData, int count)
+        {
+            //if (xData > 30)
+                chrAcc.Series["X"].Points.AddXY(count, xData);
+            //else
+                //chart.Series["xData"].Points.Clear();
+
+            //if (checkY.Checked && yData > 30)
+                chrAcc.Series["Y"].Points.AddXY(count, yData);
+            //else
+                //chrAcc.Series["yData"].Points.Clear();
+
+            //if (checkZ.Checked && zData > 30)
+                chrAcc.Series["Z"].Points.AddXY(count, zData);
+            //else
+                //chrAcc.Series["zData"].Points.Clear();
+
+            chrAcc.ChartAreas["ChartArea1"].AxisX.Maximum = count;
+            chrAcc.ChartAreas["ChartArea1"].AxisX.Minimum = count - 50;
+            chrAcc.ChartAreas["ChartArea1"].AxisY.Maximum = 255;
+            chrAcc.ChartAreas["ChartArea1"].AxisY.Minimum = 0;
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -112,24 +260,79 @@ namespace Bomberman
                 {
                     dataQueue.TryDequeue(out data);
                 }
-                // display x, y, z
-                for (Pos curr =  Pos.X; curr != Pos.OVER; curr++)
+
+                // retrieve x, y, z data, show in textbox and add to list
+                int avgCount; // how many to average
+                count++; // for x-axis of plot
+                try
+                {
+                    avgCount = int.Parse(txtN.Text);
+                }
+                catch (System.FormatException)
+                {
+                    avgCount = 0;
+                }
+                
+                for (READ_ACC curr =  READ_ACC.X; curr != READ_ACC.OVER; curr++)
                 {
                     dataQueue.TryDequeue(out data);
                     switch (curr)
                     {
-                        case Pos.X:
+                        case READ_ACC.X:
                             txtX.Text = data.ToString();
+                            while (x_acc.Count > avgCount)
+                                x_acc.RemoveAt(0);
+                            x_acc.Add(data);
                             break;
-                        case Pos.Y:
+                        case READ_ACC.Y:
                             txtY.Text = data.ToString();
+                            while (y_acc.Count > avgCount)
+                                y_acc.RemoveAt(0);
+                            y_acc.Add(data);
                             break;
-                        case Pos.Z:
+                        case READ_ACC.Z:
                             txtZ.Text = data.ToString();
+                            while (z_acc.Count > avgCount)
+                                z_acc.RemoveAt(0);
+                            z_acc.Add(data);
                             break;
                     }
                 }
 
+                // retreive last x,y,z data
+                int xData = x_acc[x_acc.Count - 1];
+                int yData = y_acc[y_acc.Count - 1];
+                int zData = z_acc[z_acc.Count - 1];
+
+                // read threshold values
+                int xThreshold, yThreshold, zThreshold;
+                try
+                {
+                    xThreshold = int.Parse(txtXThreshold.Text);
+                    yThreshold = int.Parse(txtYThreshold.Text);
+                    zThreshold = int.Parse(txtZThreshold.Text);
+                }
+                catch (System.FormatException)
+                {
+                    xThreshold = 220;
+                    yThreshold = 220;
+                    zThreshold = 220;
+                }
+                
+
+                // start gesture reading
+                if ((xData > xThreshold || zData > zThreshold) && gestureState == GESTURE.IDLE)
+                {
+                    gestureState = GESTURE.START;
+                }
+
+                Update_orientation(xData, yData, zData);
+                Calculate_average(avgCount);
+                Update_gesture(xData, yData, zData, xThreshold, yThreshold, zThreshold);
+                Update_plot(xData, yData, zData, count);
+
+                while (dataQueue.Count > itemLimit)
+                    dataQueue.TryDequeue(out int trash);
             }
         }
     }
